92b702f70927f4a663b3c4b96fab6b7a2959da29
Jeff_ZYC <jeff_zyc>
2017-07-02 23:03:09 +0800

    测试git提交到github

--- src/main/java/com/jeff/springbootdemo/Application.java
+++ src/main/java/com/jeff/springbootdemo/Application.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class Application   {
 
     public static void main(String[] args) {
+        System.out.println("测试");
         SpringApplication.run(Application.class, args);
     }
 
